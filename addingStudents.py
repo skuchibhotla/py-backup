79d44526630ef5dbb06b13f99f5b8108e31a261e
Shashank Srivatsa Kuchibhotla <sk.kuchibhotla@gmail.com>
2018-06-19 20:29:23 -0400

    Opening, reading, and writing files

--- addingStudents.py
+++ addingStudents.py
@@ -4,7 +4,7 @@ students = []
 def get_students_titlecase():
     students_titlecase = []
     for student in students:
-        students_titlecase = student["name"].title()
+        students_titlecase.append(student["name"].title())
     return students_titlecase
 
 
@@ -37,11 +37,13 @@ def read_file():
         print("Could not read file!")
 
 
-student_list = get_students_titlecase()
+# calling read_file()
+read_file()
+print_students_titlecase()
+
 
 student_name = input("Enter student name: ")
 student_id = input("Enter student ID: ")
 
 add_student(student_name, student_id)
-
-print_students_titlecase()
+save_file(student_name)
